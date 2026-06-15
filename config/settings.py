cc23b1d444ea2f63254a7a44428fb1a29e4ee3f9
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-06-15 10:13:41 +0300

    Add STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -20,9 +20,8 @@ import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# SECURITY WARNING: don't run with debug turned on in product
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     '.onrender.com'           ]
 
