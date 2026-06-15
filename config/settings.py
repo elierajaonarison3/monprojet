e5762f6228a7347ddf683ab1cbbf2b5b77ac92d2
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-06-15 11:04:27 +0300

    add hosts

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in product
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
-    'https://monprojet1-ke8b.onrender.com'
+    "https://monprojet1-ke8b.onrender.com",
+    "localhost",
+    "127.0.0.1"
     ]
 
 
