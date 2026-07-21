b22050406e7390cf4bfe7c8b732b6a9741d27c1e
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-07-21 15:25:25 +0300

    add api home route

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({
+        "message": "API Marche Public fonctionne",
+        "status": "OK"
+    })
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
+    path('api/', include('marches.urls')),
 ]
