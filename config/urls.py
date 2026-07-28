d5c81e3a3f1e6469fb61178dd13b62b6ba37ddf2
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-07-28 14:28:20 +0300

    28juillet

--- config/urls.py
+++ config/urls.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
-
 def home(request):
     return JsonResponse({
         "message": "API Marche Public fonctionne",
         "status": "OK"
     })
 
-
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('authentication.urls')),
 ]
