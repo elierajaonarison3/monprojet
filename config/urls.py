ca151b40816da98dc58b5bfcc61fd67775d00f61
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-09-14 16:39:09 +0300

    Fix media PDF s5erving

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 from authentication.views import DatabaseTestView
 from authentication.views import InitialiserComptesView
@@ -22,12 +21,9 @@ urlpatterns = [
     path('api/', include('authentication.urls')),
     path('api/test-db/', DatabaseTestView.as_view()),
     path('api/initialiser-comptes/', InitialiserComptesView.as_view()),
-
-    re_path(
-        r'^media/(?P<path>.*)$',
-        serve,
-        {
-            'document_root': settings.MEDIA_ROOT,
-        }
-    ),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
