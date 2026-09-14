8ad1b71590b2cf8b8d7a98814ef69ea671816698
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-09-14 16:05:32 +0300

    pdf configure

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import static
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -19,6 +20,4 @@ urlpatterns = [
     path('api/initialiser-comptes/', InitialiserComptesView.as_view()),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
