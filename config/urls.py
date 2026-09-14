58bf4c3c6662c01411917fc8147afeec7a3b76b1
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-09-14 16:11:13 +0300

    pdf configure

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,19 @@
-from django.conf.urls import static
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
 from authentication.views import DatabaseTestView
 from authentication.views import InitialiserComptesView
-from config import settings
+
 
 def home(request):
     return JsonResponse({
         "message": "API Marche Public fonctionne",
         "status": "OK"
     })
+
 
 urlpatterns = [
     path('', home),
@@ -20,4 +23,7 @@ urlpatterns = [
     path('api/initialiser-comptes/', InitialiserComptesView.as_view()),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
