e397e0f8a4f628c3d94a4f5730b010614b8c1519
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-09-14 17:22:16 +0300

    Configure Cloudinary PDF st5orage

--- config/settings.py
+++ config/settings.py
@@ -183,3 +183,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
