2cb6f95dff1fb84788a9ce2be8d7a7e2d55c780b
elierajaonarison3 <elierajaonarison3@gmail.com>
2026-09-14 16:25:38 +0300

    Fix media PDF serving

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from authentication.views import DatabaseTestView
 from authentication.views import InitialiserComptesView
@@ -21,9 +22,12 @@ urlpatterns = [
     path('api/', include('authentication.urls')),
     path('api/test-db/', DatabaseTestView.as_view()),
     path('api/initialiser-comptes/', InitialiserComptesView.as_view()),
-]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        }
+    ),
+]
